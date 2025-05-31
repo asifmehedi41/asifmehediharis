66d8f2927e08e7713352c173433a4f8b15f2263b
asifmehedi41 <haris2041@cseku.ac.bd>
2025-05-31 13:56:51 +0600

    Default theme light

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,7 +4,7 @@ import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider defaultTheme="dark" {...props}>
+    <NextThemesProvider defaultTheme="light" {...props}>
       {children}
     </NextThemesProvider>
   )
